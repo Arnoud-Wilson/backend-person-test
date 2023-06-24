8c5439bbd30f62dc4e5dd769b5fac54ad2abeef6
Arnoud-Wilson <arnoud608@gmail.com>
2023-06-24 10:38:01 +0200

    added lastName upperCase test and childAdded test

--- src/test/java/nl/novi/PersonTest.java
+++ src/test/java/nl/novi/PersonTest.java
@@ -1,6 +1,10 @@
 package nl.novi;
 
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
@@ -16,7 +20,7 @@ public class PersonTest {
     }
 
     @Test
-    void ageShouldAddOneYear() {
+    void testAgeShouldAddOneYear() {
         //Given (arrange)
         Person pieterPost = new Person("Pieter");
         pieterPost.setAge(40);
@@ -27,7 +31,32 @@ public class PersonTest {
         //Then (assert)
         assertEquals(41, pieterPost.getAge());
     }
+    @Test
+    void testChildShouldBeAdded() {
+        //Given (arrange)
+        Person pieterPost = new Person("Pieter");
+        Person childPost = new Person("Child Post");
+        List<Person> children = new ArrayList<>();
 
+        //When (act)
+        pieterPost.addChild(childPost);
+        children.add(childPost);
+
+        //Then (assert)
+        assertEquals(children, pieterPost.getChildren());
+    }
+
+    @Test
+    void testLastNameShouldStartWithUpperCaseWhenLowerCaseWasGiven() {
+        //Given (arrange)
+        Person pieterPost = new Person("Pieter");
+
+        //When (act)
+        pieterPost.setLastName("post");
+
+        //Then (assert)
+        assertEquals("Post", pieterPost.getLastName());
+    }
 
 }
 
