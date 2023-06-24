bf04f9132f2cab249e434dc61a4ee9f12af32eef
Arnoud-Wilson <arnoud608@gmail.com>
2023-06-24 10:22:20 +0200

    added ageAdd test and noName test

--- src/test/java/nl/novi/PersonTest.java
+++ src/test/java/nl/novi/PersonTest.java
@@ -1,4 +1,34 @@
 package nl.novi;
 
+import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PersonTest {
+
+    @Test
+    void testPersonWithoutNameShouldBeNamedJohn() {
+        //Given (arrange)
+        Person noName = new Person();
+        //When (act)
+
+        //Then (assert)
+        assertEquals("John", noName.getName());
+    }
+
+    @Test
+    void ageShouldAddOneYear() {
+        //Given (arrange)
+        Person pieterPost = new Person("Pieter");
+        pieterPost.setAge(40);
+
+        //When (act)
+        pieterPost.age();
+
+        //Then (assert)
+        assertEquals(41, pieterPost.getAge());
+    }
+
+
 }
+
+//TODO: Zie lesopdracht afbeelding in assets.
